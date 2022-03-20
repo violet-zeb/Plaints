4073dac2edffa3c1aea9a6be7a13500802eea73b
Stef PC <Stef PC@DESKTOP-VAA867M>
2022-03-20 15:25:59 +0100

    Add project files.

---
+++ Plaints/Plaints/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+[assembly: ExportFont("Lora-Regular.ttf", Alias = "LoraRegular")]
+[assembly: ExportFont("Lora-Medium.ttf", Alias = "LoraMedium")]
+
+namespace Plaints
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Plaints/Plaints/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plaints.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+    }
+}
