b5777e6fc1cd8d1c5c443c6f6baf701fae6fae06
Stef PC <Stef PC@DESKTOP-VAA867M>
2022-03-21 20:13:33 +0100

    added changes to development branch

--- Plaints/Plaints/ViewModels/MainViewModel.cs
+++ Plaints/Plaints/ViewModels/MainViewModel.cs
@@ -7,5 +7,45 @@ namespace Plaints.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        public IList<ProductViewModel> Products { get; set; }
+
+        public MainViewModel()
+        {
+            Products = new ObservableRangeCollection<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Name = "Breakfast",
+                    HeroColor = "#96C9F8",
+                    ImageUrl = "",
+                    Price = 12,
+                    IsFeatured = true
+                },
+                new ProductViewModel()
+                {
+                    Name = "Lunch",
+                    HeroColor = "#FFCA81",
+                    ImageUrl = "",
+                    Price = 17,
+                    IsFeatured = true
+                },
+                new ProductViewModel()
+                {
+                    Name = "Snack",
+                    HeroColor = "#D69DFB",
+                    ImageUrl = "",
+                    Price = 19,
+                    IsFeatured = false
+                },
+                new ProductViewModel()
+                {
+                    Name = "Dinner",
+                    HeroColor = "#74D59E",
+                    ImageUrl = "",
+                    Price = 14,
+                    IsFeatured = true
+                },
+            };
+        }
     }
 }
