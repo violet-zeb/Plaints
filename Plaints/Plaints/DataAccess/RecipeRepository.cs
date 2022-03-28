5ded849aa0497d9fa453d1f7324fe12ebdefd558
Perisa <aleksa.perisic2000@gmail.com>
2022-03-28 21:13:54 +0200

    add RecipeRepository

---
+++ Plaints/Plaints/DataAccess/RecipeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Plaints.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plaints.DataAccess
+{
+    internal class RecipeRepository : IRecipeRepository
+    {
+        public List<Recipe> GetRecipeByCategory(string category)
+        {
+            return new List<Recipe>();
+        }
+    }
+}
