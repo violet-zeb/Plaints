4b873687b2ae49a94847293b3425b540ddab1aa1
Perisa <aleksa.perisic2000@gmail.com>
2022-03-28 21:19:00 +0200

    added NavigationSevice

---
+++ Plaints/Plaints/Services/NavigationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plaints.Services
+{
+    internal class NavigationService : INavigationService
+    {
+        public void NavigateBack()
+        {
+            
+        }
+
+        public void NavigateToRecipeDetails()
+        {
+            
+        }
+
+        public void NavigateToRecipeList()
+        {
+            
+        }
+    }
+}
