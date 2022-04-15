5a08c563bae1f1836397477cc304ec7927aef715
Stef PC <Stef PC@DESKTOP-VAA867M>
2022-04-15 12:55:37 +0200

    for the 100th time, fixed app name, fixed app icon

--- Plaints/Plaints.Android/MainActivity.cs
+++ Plaints/Plaints.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Plaints.Droid
 {
-    [Activity(Label = "Plaints", Icon = "@mipmap/icon", Theme = "@style/MainTheme",  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Plaints", Icon = "@mipmap/platebox_icon", Theme = "@style/MainTheme",  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
